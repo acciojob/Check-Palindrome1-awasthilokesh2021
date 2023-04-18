b25d0b49c0cb2c928d49d6cb9e39b6cd91aa7748
awasthilokesh2021 <115865786+awasthilokesh2021@users.noreply.github.com>
2023-04-18 17:12:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
 
 function palindrome(str){
+    let str = "race a car"
 
+	let str1 = str.reverse();
+
+	if(str==str1){
+		return true
+	}
+	else{
+		return false
+	}	
 }
 module.exports = palindrome
+
+
+
